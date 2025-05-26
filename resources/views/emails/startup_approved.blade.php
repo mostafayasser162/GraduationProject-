1538009f7abd4c9e9692052933715be42048d96a
Hana Hany <hanahany@example.com>
2025-05-27 02:42:43 +0300

    commit

---
+++ resources/views/emails/startup_approved.blade.php
@@ -0,0 +1,3 @@
+<h2>Congratulations!</h2>
+<p>Your startup has been approved.</p>
+<p>Let's go and finish your profile to get started!</p>
---
+++ resources/views/emails/startup_rejected.blade.php
@@ -0,0 +1,3 @@
+<h2>We're sorry to inform you...</h2>
+<p>Your startup registration has been rejected.</p>
+<p>If you believe this was a mistake, feel free to reach out.</p>
