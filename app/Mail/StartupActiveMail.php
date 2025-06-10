36788d0ae7237cc57e6d686c1fed5adcb7173d2b
Hana Hany <hanahany@example.com>
2025-06-10 21:25:21 +0300

    commit

---
+++ app/Mail/StartupActiveMail.php
@@ -0,0 +1,24 @@
+<?php 
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class StartupActiveMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    public function build()
+    {
+        return $this->subject('Your Startup Has Been Active')
+                    ->view('emails.startup_active');
+    }
+}
